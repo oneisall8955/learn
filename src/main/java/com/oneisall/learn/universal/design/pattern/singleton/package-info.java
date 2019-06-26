aa9af97c9b47e8e01c7a804202f525191356dc89
oneisall8955 <oneisall8955@gmail.com>
2019-06-26 12:03:31 +0800

    a标签引用fix

--- src/main/java/com/oneisall/learn/universal/design/pattern/singleton/package-info.java
+++ src/main/java/com/oneisall/learn/universal/design/pattern/singleton/package-info.java
@@ -3,12 +3,12 @@
  * <p>
  * 参考文章 1:
  * <a href="https://juejin.im/post/5d08f67f51882565e833ff43">
- * https://juejin.im/post/5d08f67f51882565e833ff43
+ * happyjava:为什么我强烈推荐你用枚举来实现单例模式
  * </a>
  * <br>
  * 参考文章 2:
  * <a href="https://mp.weixin.qq.com/s?__biz=MzU5NTg2NTI0OA==&mid=2247483772&idx=1&sn=03d2db88210697186edd36ef6bb176ea">
- * https://mp.weixin.qq.com/s?__biz=MzU5NTg2NTI0OA==&mid=2247483772&idx=1&sn=03d2db88210697186edd36ef6bb176ea
+ * 秉心说:钻钻 “单例” 的牛角尖
  * </a>
  */
 package com.oneisall.learn.universal.design.pattern.singleton;
