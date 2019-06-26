1a224772575ee4d8a00bc6e79356c7e2a0095847
oneisall8955 <oneisall8955@gmail.com>
2019-06-26 12:01:20 +0800

    package-info

---
+++ src/main/java/com/oneisall/learn/universal/design/pattern/singleton/package-info.java
@@ -0,0 +1,14 @@
+/**
+ * 各种单例模式的实现及评价
+ * <p>
+ * 参考文章 1:
+ * <a href="https://juejin.im/post/5d08f67f51882565e833ff43">
+ * https://juejin.im/post/5d08f67f51882565e833ff43
+ * </a>
+ * <br>
+ * 参考文章 2:
+ * <a href="https://mp.weixin.qq.com/s?__biz=MzU5NTg2NTI0OA==&mid=2247483772&idx=1&sn=03d2db88210697186edd36ef6bb176ea">
+ * https://mp.weixin.qq.com/s?__biz=MzU5NTg2NTI0OA==&mid=2247483772&idx=1&sn=03d2db88210697186edd36ef6bb176ea
+ * </a>
+ */
+package com.oneisall.learn.universal.design.pattern.singleton;
