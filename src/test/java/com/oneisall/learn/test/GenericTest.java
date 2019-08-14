589c2f5b3fedb2da81c684c9c1b5020e05720396
oneisall8955 <oneisall8955@gmail.com>
2019-08-14 18:52:38 +0800

    super attention why

--- src/test/java/com/oneisall/learn/test/GenericTest.java
+++ src/test/java/com/oneisall/learn/test/GenericTest.java
@@ -3,6 +3,8 @@ package com.oneisall.learn.test;
 import com.google.common.collect.Lists;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -13,17 +15,24 @@ import java.util.List;
  * 博客园:RainDream:<? extends T>和<? super T>:https://www.cnblogs.com/drizzlewithwind/p/6100164.html
  * <p></p>
  * 并发编程网: 泛型中? super T和? extends T的区别 : http://ifeve.com/difference-between-super-t-and-extends-t-in-java/
+ * <p></p>
+ * stackoverflow:Difference between <? super T> and <? extends T> in Java [duplicate]:https://stackoverflow.com/questions/4343202/difference-between-super-t-and-extends-t-in-java
  * @author : oneisall
  * @version : v1 2019/8/13 09:29
  */
 @SuppressWarnings("all")
 public class GenericTest {
-    class Fruit {
+
+    class Food {
         protected String name = "Fruit";
 
         public void echo() {
             System.out.println(name);
         }
+    }
+
+    class Fruit extends Food {
+
     }
 
     class Apple extends Fruit {
@@ -58,7 +67,7 @@ public class GenericTest {
         }
 
         public void methodWithT(T t) {
-            System.out.println("methodWithT,T's address is : " + Integer.toHexString(t.hashCode()));
+            System.out.println("methodWithT,T's is : " + t.toString());
         }
     }
 
@@ -263,7 +272,7 @@ public class GenericTest {
          Plate<Pear> pearPlate=new Plate<Pear>();
          pearPlate.set(apple);
          pearPlate.methodWithT(apple);
-         明显类型不相同,导致编译异常
+         明显类型不相同,且不符合多态,导致编译异常
 
          /// ERROR:
          // extendsFruitPlate.set(pear);
@@ -272,7 +281,172 @@ public class GenericTest {
          Plate<Apple> pearPlate=new Plate<Apple>();
          pearPlate.set(pear);
          pearPlate.methodWithT(pear);
-         明显类型不相同,导致编译异常
+         明显类型不相同,且不符合多态,导致编译异常
          * */
     }
+
+    /**
+     * <? supper T> 下界通配符(Lower Bounds Wildcards)
+     * 注意点: 取出是Object,存放是父类或子类
+     */
+    @Test
+    public void superAttentionTest() {
+        Object object = new Object();
+        Food food = new Food();
+        Fruit fruit = new Fruit();
+        Apple apple = new Apple();
+        Pear pear = new Pear();
+
+        Plate<? super Fruit> superFruitPlate;
+        // 可以被 Plate<Object> , Plate<Food> ,Plate<Fruit> Plate<父类容器> 赋值
+        superFruitPlate = new Plate<Object>(object);
+        superFruitPlate = new Plate<Food>();
+        superFruitPlate = new Plate<Fruit>();
+        // ERROR 非Fruit及Fruit父类容器赋值编译不通过
+        // superFruitPlate = new Plate<Apple>();
+        // superFruitPlate = new Plate<Pear>();
+
+        // SUCCESS
+        superFruitPlate.set(fruit);
+        superFruitPlate.set(apple);
+        superFruitPlate.set(pear);
+        superFruitPlate.methodWithT(fruit);
+        superFruitPlate.methodWithT(apple);
+        superFruitPlate.methodWithT(pear);
+        // ERROR:调用method(T t)时候,当t[不是 T或T的子类时],则编译异常
+        // superFruitPlate.set(food);
+        // superFruitPlate.set(object);
+        // superFruitPlate.methodWithT(food);
+        // superFruitPlate.methodWithT(object);
+
+        System.out.println("=======================");
+
+
+        // 只能获取到Object对象,需要进行强转才可以进行调用相关API
+        Object supperFruitPlateGet = superFruitPlate.get();
+        if (supperFruitPlateGet instanceof Fruit) {
+            // 为什么需要 instanceof ? superFruitPlate可以指向Plate<Object>,获取出来是Object
+            Fruit convertFruit = (Fruit) supperFruitPlateGet;
+            convertFruit.echo();
+        }
+
+        // 使用List重现
+        List<Object> objects = new ArrayList<>(Arrays.asList(object));
+        List<Food> foods = new ArrayList<>(Arrays.asList(food));
+        List<Fruit> fruits = new ArrayList<>(Arrays.asList(fruit));
+        List<Apple> apples = new ArrayList<>(Arrays.asList(apple));
+        List<Pear> pears = new ArrayList<>(Arrays.asList(pear));
+        List<? super Fruit> superFruitList;
+        // 可以被 List<Object> , List<Food> ,List<Fruit> List<父类容器> 赋值
+        superFruitList = fruits;
+        superFruitList = foods;
+        superFruitList = objects;
+        // ERROR 非Fruit及Fruit父类容器则编译不通过
+        // superFruitList = apples;
+        // superFruitList = pears;
+
+        // SUCCESS 添加
+        superFruitList.add(fruit);
+        superFruitList.add(apple);
+        superFruitList.add(pear);
+        // 原理一样,使用List重现
+        // ERROR:调用add(T t)时候,当t[不是 Fruit或Fruit的子类时],则编译异常
+        // superFruitList.add(object);
+        // superFruitList.add(food);
+
+
+        // SUCCESS:调用 T get(int index),只能获取到Object对象,需要进行强转才可以进行调用相关API
+        Object getByFruitPlate = superFruitList.get(0);
+        if (getByFruitPlate instanceof Fruit) {
+            // 为什么需要 instanceof ? superFruitList可以指向List<Object> objects,获取出来是Object
+            Fruit convertFruit = (Fruit) getByFruitPlate;
+            convertFruit.echo();
+        } else {
+            System.out.println("非Fruit,插入非本类或非子类:" + getByFruitPlate);
+        }
+
+        System.out.println("=======================");
+
+        // 接口测试
+        class SuperClass {
+            public void supperMethod(List<? super Fruit> superList) {
+
+                superList.add(fruit);
+                superList.add(apple);
+                superList.add(pear);
+                // ERROR:原因如上,调用method(T t)时候,当t[不是 T或T的子类时],则编译异常
+                // superList.add(object);
+                // superList.add(food);
+                Object innerObject = superList.get(0);
+                if (innerObject instanceof Fruit) {
+                    // 为什么需要 instanceof ? 像这样:superFruitPlate 可以指向List<Object> objects,获取出来是Object
+                    Fruit innerConvertFruit = (Fruit) innerObject;
+                    innerConvertFruit.echo();
+                } else {
+                    System.out.println("SuperClass,supperMethod:非Fruit,插入非本类或非子类:" + innerObject);
+                }
+            }
+        }
+        SuperClass superClass = new SuperClass();
+        superClass.supperMethod(objects);
+        superClass.supperMethod(foods);
+        superClass.supperMethod(fruits);
+        // ERROR 原因同上,非Fruit及Fruit父类容器则编译不通过
+        // superClass.supperMethod(apples);
+        // superClass.supperMethod(pears);
+
+        /**
+         总结:<? super T>修饰的泛类实例,
+         为什么读取的时候,为什么是Object类型? 调用形如 method(T t)时候,t只能是T或T的子类,否则编译异常 ?
+
+         使用List接口例子理解:
+
+         List<Object> objects = new ArrayList<>(Arrays.asList(object));
+         List<Food> foods = new ArrayList<>(Arrays.asList(food));
+         List<Fruit> fruits = new ArrayList<>(Arrays.asList(fruit));
+         List<? super Fruit> superFruitList;
+         // 可以被 List<Object> , List<Food> ,List<Fruit> List<父类容器> 赋值
+         superFruitList = fruits;
+         superFruitList = foods;
+         superFruitList = objects;
+
+         编译器的理解: List<? super Fruit> superFruitList 这个 List ,
+         superFruitList不能确保读取到Fruit,因为superFruitList可能指向List<Object> objects或List<Food> foods
+         superFruitList不能确保读取到Food,因为superFruitList可能指向List<Object>
+         所以,取出来必须是[Object],最后需要则调用强转
+
+         你不能插入一个 Food 元素，因为 superFruitList 可能指向 List<Fruit>
+         你不能插入一个 Object 元素，因为 superFruitList 可能指向 List<Fruit> 或 List<Food>
+
+         你可以插入一个 Fruit/Apple/Pear 类型的元素,因为 Fruit/Apple/Pear 类都是 Fruit,Food,Object的本身或子类
+
+         所以,你从superFruitList获取到的都是Object对象;调用形如 method(T t)时候,t只能是T或T的子类,否则编译异常
+
+         所以,当你理解以上编译器的规则,我们自定义的Plate泛类的例子自然也能理解了:
+         Plate<? super Fruit> superFruitPlate;
+         superFruitPlate = new Plate<Object>(object);
+         superFruitPlate = new Plate<Food>(food);
+         superFruitPlate = new Plate<Fruit>(fruit);
+         // ERROR:
+         // superFruitPlate.set(object);
+         // superFruitPlate.methodWithT(object);
+         superFruitPlate 可能指向 Plate<Food> , 此时以上调用等价于
+         Plate<Food> pearPlate=new Plate<Food>();
+         pearPlate.set(object);
+         pearPlate.methodWithT(object);
+         明显类型不相同,且不符合多态,导致编译异常
+
+         // ERROR:
+         // superFruitPlate.set(food);
+         // superFruitPlate.methodWithT(food);
+         superFruitPlate 可能指向 Plate<Fruit> , 此时以上调用等价于
+         Plate<Food> pearPlate=new Plate<Fruit>();
+         pearPlate.set(food);
+         pearPlate.methodWithT(food);
+         明显类型不相同,且不符合多态,导致编译异常
+         * */
+
+    }
+
+
 }
