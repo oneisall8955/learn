ac2542847c70ef1981240c673045ee2a2e453c6c
LiuZhicong <930092177@qq.com>
2020-11-09 20:21:59 +0800

    T21 递归方式

--- src/main/java/com/oneisall/learn/leetcode/T21MergeTwoLists.java
+++ src/main/java/com/oneisall/learn/leetcode/T21MergeTwoLists.java
@@ -75,6 +75,20 @@ public class T21MergeTwoLists {
         return result;
     }
 
+    public static ListNode mergeTwoLists2(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        } else if (l2 == null) {
+            return l1;
+        } else if (l1.val < l2.val) {
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoLists(l1, l2.next);
+            return l2;
+        }
+    }
+
     public static void main(String[] args) {
         ListNode l1 = new ListNode(1, new ListNode(3, new ListNode(5)));
         ListNode l2 = new ListNode(2, new ListNode(4, new ListNode(6)));
